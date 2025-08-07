fe3cd50a9b6ef8bcede4ec2467ef8e3fb7820142
unver <unvercanunlu@gmail.com>
2025-08-07 20:58:47 +0200

    enums updated.

--- src/main/java/tr/unvercanunlu/parking_lot/config/AppConfig.java
+++ src/main/java/tr/unvercanunlu/parking_lot/config/AppConfig.java
@@ -6,11 +6,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AppConfig {
 
+  // price
   public static final double PRICE_START = 1.0;
   public static final double PRICE_PER_HOUR = 2.0;
   public static final double PRICE_DAILY_MAX = 40.0;
+
+  // car plate
   public static final String CAR_PLATE_REGEX = "^[A-Za-z0-9]+(-[A-Za-z0-9]+)*$";
+
+  // spot
   public static final int SPOT_NO_START = 1;
+
+  // record
   public static final long RECORD_ID_START = 1;
 
 }
--- src/main/java/tr/unvercanunlu/parking_lot/model/constant/ParkingStatus.java
+++ src/main/java/tr/unvercanunlu/parking_lot/model/constant/ParkingStatus.java
@@ -1,5 +1,9 @@
 package tr.unvercanunlu.parking_lot.model.constant;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +15,27 @@ public enum ParkingStatus {
 
   @Getter
   private final char code;
+
+  private static final Map<Character, ParkingStatus> STATUSES;
+
+  static {
+    Map<Character, ParkingStatus> map = new HashMap<>();
+
+    for (ParkingStatus status : ParkingStatus.values()) {
+      map.put(status.code, status);
+    }
+
+    STATUSES = Collections.unmodifiableMap(map);
+  }
+
+  public static Optional<ParkingStatus> of(Character code) {
+    if (code == null) {
+      throw new IllegalArgumentException("Code missing!");
+    }
+
+    return Optional.ofNullable(
+        STATUSES.get(code)
+    );
+  }
 
 }
